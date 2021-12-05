e213c3e84a28811f1bcff585d952ee84920c90eb
Joseph <j.collora@outlook.com>
2021-12-04 22:22:36 -0800

    updated/semi-final comments for patron

--- patron.cpp
+++ patron.cpp
@@ -20,28 +20,93 @@
 
 using namespace std;
 
+// -------------------------------------------------------------------------
+/** Patron()
+ * Default Constructor
+ *
+ * Creates a Patron object. Member variables are instantiated to default
+ * values. Book requires a book factory in order to be properly instantiated
+ * with data
+ * @pre None.
+ * @post Patron object exists with default member variables
+ */
 Patron::Patron() {
    id = "";
    lastName = "";
    firstName = "";
-   
 }
 
+// -------------------------------------------------------------------------
+/** Patron(ID)
+ * Constructor with ID
+ * 
+ * Alternate constructor for patron that instantiates ID field to the given 
+ * string value.
+ * 
+ * @param id string of characters that make up the desired patron ID
+ * @pre None.
+ * @post Patron object exists with given ID value
+ */
 Patron::Patron(string newID) {
    id = newID;
-
+   lastName = "";
+   firstName = "";
 }
 
+// -------------------------------------------------------------------------
+/** ~Patron()
+ * Destructor
+ * 
+ * Deletes Patron from memory. It's empty, but apparently helps to delete
+ * strings
+ * @pre None.
+ * @post Patron instance is deleted
+ */
 Patron::~Patron() {}
 
-bool Patron::addBook(Book* book)
-{
+// -------------------------------------------------------------------------
+/** compare()
+ * Compare patrons
+ *
+ * Compare 2 patrons. Returns an integer that reflects the comparison
+ * @pre None
+ * @post new patron object exists
+ * @return negative int if left patron < right.
+ * return 0 if equal, return positive int if left > right
+ */
+int Patron::compare(const Patron& rhs) const {
+   int comparison = id.compare(rhs.id);
+
+   return comparison;
+}
+
+// -------------------------------------------------------------------------
+/** addBook()
+ * Add book to currentCheckouts
+ * @param book the book to checkout
+ * @pre Book object must exist and should have available copies, 
+ * Book's count will need to be updated if this is successful
+ * @post if successful, the book is added to the set of currentCheckouts
+ * (notes for command: book will decrease in count if success, book points 
+ * to patron command
+ * will add itself to the patron command history if success)
+ * @return True only if patron does not already have the book
+ */
+bool Patron::addBook(Book* book) {
    currentCheckouts[book]++;
    return true;
 }
 
-bool Patron::removeBook(Book* book)
-{
+// -------------------------------------------------------------------------
+/** removeBook()
+ * @param book the book to return
+ * @pre Book should exist
+ * @post If successful, book is removed from currentCheckouts.
+ * (notes for comand: book count++, book no longer points to patron,
+ *  command adds itself to patron)
+ * @return true only if Patron had the book checked out
+ */
+bool Patron::removeBook(Book* book) {
    if (currentCheckouts[book] <= 0) {
       cout << "ERROR: Patron " << id
            << "tried to return a book it did not have titled "
@@ -53,62 +118,107 @@ bool Patron::removeBook(Book* book)
    return true;
 }
 
-string Patron::displayPatron() const
-{
-   return firstName + " " + lastName; // Finish during commands
-}
-
-string Patron::displayPatronHistory() const
-{
-   return "Book"; // Finish during commands
-}
-
-int Patron::compare(const Patron& rhs) const
-{
-   int comparison = id.compare(rhs.id);
-
-   return comparison;
-}
-
-bool Patron::operator<(const BSTData& rhs) const
-{
+// -------------------------------------------------------------------------
+/** operator<()
+ * Operator less than overload
+ *
+ * Compares this patron to right-hand patron to see if this is less than rhs
+ * @param rhs patron to be compared
+ * @pre None.
+ * @post None. const
+ * @return true if this patron is less than rhs, else false
+ */
+bool Patron::operator<(const BSTData& rhs) const {
    const Patron& right = static_cast<const Patron&>(rhs);
    return compare(right) < 0;
 }
 
-bool Patron::operator>(const BSTData& rhs) const
-{
+// -------------------------------------------------------------------------
+/** operator>()
+ * Operator greater than overload
+ *
+ * Compares this patron to right-hand patron to see if this is greater than 
+ * rhs
+ * @param rhs patron to be compared
+ * @pre None.
+ * @post None. const
+ * @return true if this patron is greater than rhs, else false
+ */
+bool Patron::operator>(const BSTData& rhs) const {
    const Patron& right = static_cast<const Patron&>(rhs);
    return compare(right) > 0;
 }
 
-bool Patron::operator==(const BSTData& rhs) const
-{
+// -------------------------------------------------------------------------
+/** operator==()
+ * Operator equal to overloadd
+ *
+ * Compares this patron to right-hand patron to see if this is equal to rhs
+ * @param rhs patron to be compared
+ * @pre None.
+ * @post None. const
+ * @return true if this patron is equal to rhs, else false
+ */
+bool Patron::operator==(const BSTData& rhs) const {
    const Patron& right = static_cast<const Patron&>(rhs);
    return compare(right) == 0;
 }
 
-bool Patron::operator!=(const BSTData& rhs) const
-{
+// -------------------------------------------------------------------------
+/** operator!=
+ * Inequality Operator
+ * 
+ * Determines if right and left items are not equivelent
+ * @pre Both items being compared exist
+ * @post None.
+ * @return false returned when left is equal to right, true otherwise
+ */
+bool Patron::operator!=(const BSTData& rhs) const {
    const Patron& right = static_cast<const Patron&>(rhs);
    return compare(right) != 0;
 }
 
-bool Patron::operator<=(const BSTData& rhs) const
-{
+// -------------------------------------------------------------------------
+/** operator<=()
+ *  Less than or equal operator
+ * 
+ * Determines if the right and left items are equivelent or less than
+ * @pre Both items being compared exist
+ * @post None.
+ * @return true returned when left is equal to right or
+ * if left is less than right, false otherwise
+ */
+bool Patron::operator<=(const BSTData& rhs) const {
    const Patron& right = static_cast<const Patron&>(rhs);
    return compare(right) <= 0;
 }
 
-bool Patron::operator>=(const BSTData& rhs) const
-{
+// -------------------------------------------------------------------------
+/** operator>=()
+ *  greater than or equal operator
+ * 
+ * Determines if the right and left items are equivelent or greater than
+ * @pre Both items being compared exist
+ * @post None.
+ * @return true returned when left is equal to right or
+ * if left is greater than right, false otherwise
+ */
+bool Patron::operator>=(const BSTData& rhs) const {
 
    const Patron& right = static_cast<const Patron&>(rhs);
    return compare(right) >= 0;
 }
 
-BSTData& Patron::operator=(const BSTData& rhs)
-{
+// -------------------------------------------------------------------------
+/** operator=()
+ * Copy assignment operator
+ * 
+ * Copy data from right hand item to left hand item.
+ * @pre Items should not be the same item
+ * @post left item contains data from rhs, right item is const
+ * @return reference to left item
+ */
+BSTData& Patron::operator=(const BSTData& rhs) {
    const Patron& right = static_cast<const Patron&>(rhs);
    if (this != &right) {
       id = right.id;
@@ -121,8 +231,16 @@ BSTData& Patron::operator=(const BSTData& rhs)
    return *this;
 }
 
-bool Patron::setData(istream& is)
-{
+// -------------------------------------------------------------------------
+/** setData()
+ * input data into node
+ * 
+ * Take data from inputStream and put into node members
+ * @pre must have a properly formatted input file
+ * @post line of input is read. BSTData contains line data
+ * @return true if line of data was read, false if no line or bad format
+ */
+bool Patron::setData(istream& is) {
    if (is.eof()) {
       return false;
    }
@@ -142,8 +260,16 @@ bool Patron::setData(istream& is)
    return true;
 }
 
-ostream& Patron::display(ostream& os) const
-{
+// -------------------------------------------------------------------------
+/** display()
+ * display data
+ * 
+ * returns the data inside node as a string
+ * @pre None
+ * @post None
+ * @return string representing data inside node
+ */
+ostream& Patron::display(ostream& os) const {
    os.setf(ios::left, ios::adjustfield);
    os << id << " " << lastName << ", " << firstName << ":" << endl;
    for (const LibraryCommand* comm : commandHistory) {
@@ -154,10 +280,29 @@ ostream& Patron::display(ostream& os) const
    return os;
 }
 
+// -------------------------------------------------------------------------
+/** getID()
+ * get ID
+ * 
+ * returns the string id of the current patron instance
+ * @pre None
+ * @post None
+ * @return string representing patron's unique ID 
+ */
 string Patron::getID() const { return id; }
 
-void Patron::addCommand(LibraryCommand* command)
-{
+// -------------------------------------------------------------------------
+/** addCommand
+ * add command 
+ * 
+ * Adds the command item to the forward list of commands that represent the
+ * current patron's history of attempted commands
+ * @param command command item to be inserted into the patron's history
+ * @pre None
+ * @post command item is properly inserted into the history forward_list
+ * @return None
+ */
+void Patron::addCommand(LibraryCommand* command) {
    // insert at the end of the command history singly linked list
    commandHistory.insert_after(commandHistory.end(), command);
 }
--- patron.h
+++ patron.h
@@ -19,7 +19,6 @@
 using namespace std;
 
 #include "constants.h"
-
 #include "BSTData.h"
 
 #include <iostream>
@@ -30,211 +29,224 @@ using namespace std;
 class LibraryCommand;
 class Book;
 
-
 class Patron : public BSTData 
 {
 public:
-    // -------------------------------------------------------------------------
+   // -------------------------------------------------------------------------
    /** Patron()
-     * Default Constructor
-     *
-     * Creates a Patron object. Member variables are instantiated to default
-     * values. Book requires a book factory in order to be properly instantiated
-     * with data
-     * @pre None.
-     * @post Patron object exists with default member variables
+    * Default Constructor
+    *
+    * Creates a Patron object. Member variables are instantiated to default
+    * values. Book requires a book factory in order to be properly instantiated
+    * with data
+    * @pre None.
+    * @post Patron object exists with default member variables
     */
    Patron();
 
+   // -------------------------------------------------------------------------
+   /** Patron(ID)
+    * Constructor with ID
+    * 
+    * Alternate constructor for patron that instantiates ID field to the given 
+    * string value.
+    * 
+    * @param id string of characters that make up the desired patron ID
+    * @pre None.
+    * @post Patron object exists with given ID value
+    */
    Patron(string id);
 
    // -------------------------------------------------------------------------
-    /** ~Patron()
-     * Destructor
-     * 
-     * Deletes Patron from memory. It's empty, but apparently helps to delete
-     * strings
-     * @pre None.
-     * @post Patron instance is deleted
+   /** ~Patron()
+    * Destructor
+    * 
+    * Deletes Patron from memory. It's empty, but apparently helps to delete
+    * strings
+    * @pre None.
+    * @post Patron instance is deleted
     */
    virtual ~Patron();
 
-    // -------------------------------------------------------------------------
-    /** addBook()
-     * Add book to currentCheckouts
-     * @param book the book to checkout
-     * @pre Book object must exist and should have available copies, 
-     * Book's count will need to be updated if this is successful
-     * @post if successful, the book is added to the set of currentCheckouts
-     * (notes for command: book will decrease in count if success, book points 
-     * to patron command
-     * will add itself to the patron command history if success)
-     * @return True only if patron does not already have the book
-     */
+   // -------------------------------------------------------------------------
+   /** addBook()
+    * Add book to currentCheckouts
+    * @param book the book to checkout
+    * @pre Book object must exist and should have available copies, 
+    * Book's count will need to be updated if this is successful
+    * @post if successful, the book is added to the set of currentCheckouts
+    * (notes for command: book will decrease in count if success, book points 
+    * to patron command
+    * will add itself to the patron command history if success)
+    * @return True only if patron does not already have the book
+    */
     bool addBook(Book* book);
 
-    // -------------------------------------------------------------------------
-    /** removeBook()
-     * @param book the book to return
-     * @pre Book should exist
-     * @post If successful, book is removed from currentCheckouts.
-     * (notes for comand: book count++, book no longer points to patron,
-     *  command adds itself to patron)
-     * @return true only if Patron had the book checked out
-     */
-   bool removeBook(Book* book);
+  // -------------------------------------------------------------------------
+  /** removeBook()
+   * @param book the book to return
+   * @pre Book should exist
+   * @post If successful, book is removed from currentCheckouts.
+   * (notes for comand: book count++, book no longer points to patron,
+   *  command adds itself to patron)
+   * @return true only if Patron had the book checked out
+   */
+  bool removeBook(Book* book);
 
-    // -------------------------------------------------------------------------
-    /** displayPatron()
-     * @pre None.
-     * @post None. const function
-     * @return "ID   LastName, FirstName" as a string
-     */
-   string displayPatron() const;
+  // -------------------------------------------------------------------------
+  /** operator<()
+   * Operator less than overload
+   *
+   * Compares this patron to right-hand patron to see if this is less than rhs
+   * @param rhs patron to be compared
+   * @pre None.
+   * @post None. const
+   * @return true if this patron is less than rhs, else false
+   */
+  virtual bool operator<(const BSTData& rhs) const;
 
-// -------------------------------------------------------------------------
-    /** displayPatronHistory()
-     * @pre None.
-     * @post None. const function
-     * @return string of commands in commandHistory
-     */  
-    string displayPatronHistory() const;
+  // -------------------------------------------------------------------------
+  /** operator>()
+   * Operator greater than overload
+   *
+   * Compares this patron to right-hand patron to see if this is greater than 
+   * rhs
+   * @param rhs patron to be compared
+   * @pre None.
+   * @post None. const
+   * @return true if this patron is greater than rhs, else false
+   */
+  virtual bool operator>(const BSTData& rhs) const;
 
-// -------------------------------------------------------------------------
-   /** operator<()
-    * Operator less than overload
-    *
-    * Compares this patron to right-hand patron to see if this is less than rhs
-    * @param rhs patron to be compared
-    * @pre None.
-    * @post None. const
-    * @return true if this patron is less than rhs, else false
-    */
-   virtual bool operator<(const BSTData& rhs) const;
+  // -------------------------------------------------------------------------
+  /** operator==()
+   * Operator equal to overloadd
+   *
+   * Compares this patron to right-hand patron to see if this is equal to rhs
+   * @param rhs patron to be compared
+   * @pre None.
+   * @post None. const
+   * @return true if this patron is equal to rhs, else false
+   */
+  virtual bool operator==(const BSTData& rhs) const;
 
-   // -------------------------------------------------------------------------
-   /** operator>()
-    * Operator greater than overload
-    *
-    * Compares this patron to right-hand patron to see if this is greater than 
-    * rhs
-    * @param rhs patron to be compared
-    * @pre None.
-    * @post None. const
-    * @return true if this patron is greater than rhs, else false
-    */
-   virtual bool operator>(const BSTData& rhs) const;
+  // -------------------------------------------------------------------------
+  /** operator!=
+   * Inequality Operator
+   * 
+   * Determines if right and left items are not equivelent
+   * @pre Both items being compared exist
+   * @post None.
+   * @return false returned when left is equal to right, true otherwise
+   */
+  virtual bool operator!=(const BSTData& rhs) const;
 
-   // -------------------------------------------------------------------------
-   /** operator==()
-    * Operator equal to overloadd
-    *
-    * Compares this patron to right-hand patron to see if this is equal to rhs
-    * @param rhs patron to be compared
-    * @pre None.
-    * @post None. const
-    * @return true if this patron is equal to rhs, else false
-    */
-   virtual bool operator==(const BSTData& rhs) const;
+  // -------------------------------------------------------------------------
+  /** operator<=()
+   *  Less than or equal operator
+   * 
+   * Determines if the right and left items are equivelent or less than
+   * @pre Both items being compared exist
+   * @post None.
+   * @return true returned when left is equal to right or
+   * if left is less than right, false otherwise
+   */
+  virtual bool operator<=(const BSTData& rhs) const;
 
-   // -------------------------------------------------------------------------
-   /** operator!=
-    * Inequality Operator
-    * 
-    * Determines if right and left items are not equivelent
-    * @pre Both items being compared exist
-    * @post None.
-    * @return false returned when left is equal to right, true otherwise
-    */
-   virtual bool operator!=(const BSTData& rhs) const;
+  // -------------------------------------------------------------------------
+  /** operator>=()
+   *  greater than or equal operator
+   * 
+   * Determines if the right and left items are equivelent or greater than
+   * @pre Both items being compared exist
+   * @post None.
+   * @return true returned when left is equal to right or
+   * if left is greater than right, false otherwise
+   */
+  virtual bool operator>=(const BSTData& rhs) const;
 
-   // -------------------------------------------------------------------------
-   /** operator<=()
-    *  Less than or equal operator
-    * 
-    * Determines if the right and left items are equivelent or less than
-    * @pre Both items being compared exist
-    * @post None.
-    * @return true returned when left is equal to right or
-    * if left is less than right, false otherwise
-    */
-   virtual bool operator<=(const BSTData& rhs) const;
+  // -------------------------------------------------------------------------
+  /** operator=()
+   * Copy assignment operator
+   * 
+   * Copy data from right hand item to left hand item.
+   * @pre Items should not be the same item
+   * @post left item contains data from rhs, right item is const
+   * @return reference to left item
+   */
+  virtual BSTData& operator=(const BSTData& rhs);
+    
+  // -------------------------------------------------------------------------
+  /** setData()
+   * input data into node
+   * 
+   * Take data from inputStream and put into node members
+   * @pre must have a properly formatted input file
+   * @post line of input is read. BSTData contains line data
+   * @return true if line of data was read, false if no line or bad format
+   */
+  virtual bool setData(istream& is);
 
-   // -------------------------------------------------------------------------
-   /** operator>=()
-    *  greater than or equal operator
-    * 
-    * Determines if the right and left items are equivelent or greater than
-    * @pre Both items being compared exist
-    * @post None.
-    * @return true returned when left is equal to right or
-    * if left is greater than right, false otherwise
-    */
-   virtual bool operator>=(const BSTData& rhs) const;
+  // -------------------------------------------------------------------------
+  /** display()
+   * display data
+   * 
+   * returns the data inside node as a string
+   * @pre None
+   * @post None
+   * @return string representing data inside node
+   */
+  virtual ostream& display(ostream& os) const;
 
-   
+  // -------------------------------------------------------------------------
+  /** getID()
+   * get ID
+   * 
+   * returns the string id of the current patron instance
+   * @pre None
+   * @post None
+   * @return string representing patron's unique ID 
+   */
+  string getID() const;
 
-   // -------------------------------------------------------------------------
-   /** operator=()
-    * Copy assignment operator
-    * 
-    * Copy data from right hand item to left hand item.
-    * @pre Items should not be the same item
-    * @post left item contains data from rhs, right item is const
-    * @return reference to left item
-    */
-   virtual BSTData& operator=(const BSTData& rhs);
-// -------------------------------------------------------------------------
-    /** setData()
-     * input data into node
-     * 
-     * Take data from inputStream and put into node members
-     * @pre must have a properly formatted input file
-     * @post line of input is read. BSTData contains line data
-     * @return true if line of data was read, false if no line or bad format
-     */
-    virtual bool setData(istream& is);
-
-      // -------------------------------------------------------------------------
-    /** display 
-     * display data
-     * 
-     * returns the data inside node as a string
-     * @pre None
-     * @post None
-     * @return string representing data inside node
-    */
-    virtual ostream& display(ostream& os) const;
-
-    string getID() const;
-
-    void addCommand(LibraryCommand* command);
+  // -------------------------------------------------------------------------
+  /** addCommand
+   * add command 
+   * 
+   * Adds the command item to the forward list of commands that represent the
+   * current patron's history of attempted commands
+   * @param command command item to be inserted into the patron's history
+   * @pre None
+   * @post command item is properly inserted into the history forward_list
+   * @return None
+   */
+  void addCommand(LibraryCommand* command);
 
  private:
-    // -------------------------------------------------------------------------
-    /** compare()
-     * Compare patrons
-     *
-     * Compare 2 patrons. Returns an integer that reflects the comparison
-     * @pre None
-     * @post new patron object exists
-     * @return negative int if left patron < right.
-     * return 0 if equal, return positive int if left > right
-    */
-    int compare(const Patron& rhs) const;
+  // -------------------------------------------------------------------------
+  /** compare()
+   * Compare patrons
+   *
+   * Compare 2 patrons. Returns an integer that reflects the comparison
+   * @pre None
+   * @post new patron object exists
+   * @return negative int if left patron < right.
+   * return 0 if equal, return positive int if left > right
+   */
+  int compare(const Patron& rhs) const;
 
-    // id of the patron
-    string id;
+  // id of the patron
+  string id;
 
-    // name of the patron
-    string lastName;
-    string firstName;
+  // name of the patron
+  string lastName;
+  string firstName;
 
-    // patron command history singly linked list
-    forward_list<const LibraryCommand*> commandHistory;
+  // patron command history singly linked list
+  forward_list<const LibraryCommand*> commandHistory;
 
-    //  
-    unordered_map<const Book*, int> currentCheckouts;
+  // patron current book checkouts unordered map (for multiple checkouts)
+  unordered_map<const Book*, int> currentCheckouts;
 };
 
 #endif 
